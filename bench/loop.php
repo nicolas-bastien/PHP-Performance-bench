bb4c00059c6a2a14ebd01f7c0de55689e366f86c
Nicolas Bastien <nbastien@prestaconcept.net>
2013-11-28 14:01:55 +0100

    Rework test to loop on big array and and array with string as key

--- bench/loop.php
+++ bench/loop.php
@@ -10,44 +10,57 @@
 
 function bench_loop()
 {
-    bench_foreach_array_key_numeric(10000);
+    bench_foreach_array_key_numeric(1000000);
+    bench_foreach_array_key_string(1000000);
 }
 
-function bench_foreach_array_key_numeric($max)
+function bench_loop_base(array $arrayTest)
 {
-    echo 'Bench : skeleton' . PHP_EOL;
-
-    $arrayTest = array(
-        'value_1', 'value_2', 'value_3', 'value_4', 'value_5',
-        'value_6', 'value_7', 'value_8', 'value_9', 'value_10'
-    );
-    $arrayTest = array_merge($arrayTest, $arrayTest, $arrayTest, $arrayTest, $arrayTest);
-
     $startA = microtime(true);
-    for ($i = 0; $i <= $max; $i++) {
-        foreach ($arrayTest as $value) {
-            $result = $value;
-        }
+    foreach ($arrayTest as $value) {
+        $result = $value;
     }
     $endA = microtime(true);
 
     echo ' foreach : ' . ($endA - $startA) .PHP_EOL;
 
     $startAB = microtime(true);
-    for ($i = 0; $i <= $max; $i++) {
-        foreach ($arrayTest as &$value) {
-            $result = $value;
-        }
+    foreach ($arrayTest as &$value) {
+        $result = $value;
     }
     $endAB = microtime(true);
 
     echo ' foreach with reference : ' . ($endAB - $startAB) .PHP_EOL;
 
+
+
+    $startE = microtime(true);
+    while (list($key, $value) = each($arrayTest)) {
+        $result = $value;
+    }
+    $endE = microtime(true);
+
+    echo ' while with list/each : ' . ($endE - $startE) . PHP_EOL;
+}
+
+function bench_foreach_array_key_numeric($max)
+{
+    echo 'Bench : bench_foreach_array_key_numeric' . PHP_EOL;
+
+    $arrayTest = array(
+        'value_1', 'value_2', 'value_3', 'value_4', 'value_5',
+        'value_6', 'value_7', 'value_8', 'value_9', 'value_10'
+    );
+    while (count($arrayTest) < $max) {
+        $arrayTest = array_merge($arrayTest, $arrayTest);
+    }
+    echo 'Array size : ' . count($arrayTest) . PHP_EOL;
+
+    bench_loop_base($arrayTest);
+
     $startB = microtime(true);
-    for ($i = 0; $i <= $max; $i++) {
-        for ($j = 0, $nb = count($arrayTest); $j < $nb; $j++) {
-            $result = $arrayTest[$j];
-        }
+    for ($j = 0, $nb = count($arrayTest); $j < $nb; $j++) {
+        $result = $arrayTest[$j];
     }
     $endB = microtime(true);
 
@@ -55,10 +68,8 @@ function bench_foreach_array_key_numeric($max)
 
     $startC = microtime(true);
     $nb = count($arrayTest);
-    for ($i = 0; $i <= $max; $i++) {
-        for ($j = 0; $j < $nb; $j++) {
-            $result = $arrayTest[$j];
-        }
+    for ($j = 0; $j < $nb; $j++) {
+        $result = $arrayTest[$j];
     }
     $endC = microtime(true);
 
@@ -66,34 +77,34 @@ function bench_foreach_array_key_numeric($max)
 
     $startD = microtime(true);
     $nb = count($arrayTest);
-    for ($i = 0; $i <= $max; $i++) {
-        $j = 0;
-        while ($j < $nb) {
-            $result = $arrayTest[$j];
-            $j++;
-        }
+    $j = 0;
+    while ($j < $nb) {
+        $result = $arrayTest[$j];
+        $j++;
     }
     $endD = microtime(true);
 
     echo ' while with count : ' . ($endD - $startD) . PHP_EOL;
+}
 
-    $startE = microtime(true);
-    for ($i = 0; $i <= $max; $i++) {
-        while (list($key, $value) = each($arrayTest)) {
-            $result = $value;
-        }
+function bench_foreach_array_key_string($max)
+{
+    echo 'Bench : bench_foreach_array_key_string' . PHP_EOL;
+
+    $arrayTest = array(
+        'value_1', 'value_2', 'value_3', 'value_4', 'value_5',
+        'value_6', 'value_7', 'value_8', 'value_9', 'value_10'
+    );
+    while (count($arrayTest) < $max) {
+        $arrayTest = array_merge($arrayTest, $arrayTest);
     }
-    $endE = microtime(true);
 
-    echo ' while with list/each : ' . ($endE - $startE) . PHP_EOL;
-
-    $startEB = microtime(true);
-    for ($i = 0; $i <= $max; $i++) {
-        while (list($key, $value) = each($arrayTest)) {
-            $result = $value;
-        }
+    $arrayKeyString = array();
+    while (list($key, $value) = each($arrayTest)) {
+        $arrayKeyString[$key . $value] = $value;
     }
-    $endEB = microtime(true);
+    unset($arrayTest);
+    echo 'Array size : ' . count($arrayKeyString) . PHP_EOL;
 
-    echo ' while with list/each with reference : ' . ($endEB - $startEB) . PHP_EOL;
+    bench_loop_base($arrayKeyString);
 }
